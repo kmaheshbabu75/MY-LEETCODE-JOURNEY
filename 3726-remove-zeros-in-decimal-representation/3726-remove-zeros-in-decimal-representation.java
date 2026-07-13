1ed1e72e7ebd735f91d7d5faeb05485bf13640e1
K Mahesh Babu <maheshbabu75k7@gmail.com>
2026-07-13 22:36:44 +0530

    Time: 3 ms (30.48%), Space: 42.7 MB (68.39%) - LeetHub

--- 3726-remove-zeros-in-decimal-representation/3726-remove-zeros-in-decimal-representation.java
+++ 3726-remove-zeros-in-decimal-representation/3726-remove-zeros-in-decimal-representation.java
@@ -1,17 +1,24 @@
+// class Solution {
+//     public long removeZeros(long n) {
+//         long result = 0;
+//         long place = 1;
+        
+//         while (n > 0) {
+//             long digit = n % 10;
+//             if (digit != 0) {
+//                 result = digit * place + result;
+//                 place *= 10;
+//             }
+//             n /= 10;
+//         }
+        
+//         return result;
+//     }
+// }
 class Solution {
     public long removeZeros(long n) {
-        long result = 0;
-        long place = 1;
-        
-        while (n > 0) {
-            long digit = n % 10;
-            if (digit != 0) {
-                result = digit * place + result;
-                place *= 10;
-            }
-            n /= 10;
-        }
-        
-        return result;
+        String s = Long.toString(n);
+        String filtered = s.replace("0", "");
+        return Long.parseLong(filtered);
     }
 }

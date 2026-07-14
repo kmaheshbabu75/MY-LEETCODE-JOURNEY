f2822d8bf338e57a039c4b7b4ae37664a8bf1b5d
K Mahesh Babu <maheshbabu75k7@gmail.com>
2026-07-14 18:20:53 +0530

    Time: 1 ms (100%), Space: 44.8 MB (29.06%) - LeetHub

--- 3162-find-the-number-of-good-pairs-i/3162-find-the-number-of-good-pairs-i.java
+++ 3162-find-the-number-of-good-pairs-i/3162-find-the-number-of-good-pairs-i.java
@@ -1,13 +1,19 @@
 class Solution {
     public int numberOfPairs(int[] nums1, int[] nums2, int k) {
-        int Count = 0;
+        int goodPairsCount = 0;
+        
+        // Loop through every element in the first array
         for (int i = 0; i < nums1.length; i++) {
+            // Loop through every element in the second array
             for (int j = 0; j < nums2.length; j++) {
+                
+                // Check if nums1[i] is divisible by (nums2[j] * k)
                 if (nums1[i] % (nums2[j] * k) == 0) {
-                    Count++;
+                    goodPairsCount++;
                 }
             }
         }
-        return Count;
+        
+        return goodPairsCount;
     }
 }

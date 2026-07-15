f4d69c9c268afdeb3c4594e1c21f1a6ba85d24ef
K Mahesh Babu <maheshbabu75k7@gmail.com>
2026-07-15 22:58:05 +0530

    Time: 2 ms (89.49%), Space: 43.3 MB (45.45%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.java
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.java
@@ -1,16 +1,35 @@
+// class Solution {
+//     public int arithmeticTriplets(int[] nums, int diff) {
+//         int count = 0;
+
+//         for(int i = 0; i < nums.length; i++){
+//             for(int j = i+1;j <  nums.length; j++){
+//                 for(int k = j+1; k < nums.length; k++){
+//                     if(nums[j] - nums[i] == diff && nums[k] - nums[j] == diff){
+//                         count++;
+//                     }
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// }
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
     public int arithmeticTriplets(int[] nums, int diff) {
+        Set<Integer> seen = new HashSet<>();
         int count = 0;
-
-        for(int i = 0; i < nums.length; i++){
-            for(int j = i+1;j <  nums.length; j++){
-                for(int k = j+1; k < nums.length; k++){
-                    if(nums[j] - nums[i] == diff && nums[k] - nums[j] == diff){
-                        count++;
-                    }
-                }
+        
+        for (int num : nums) {
+            // If the two previous elements of the triplet exist in the set
+            if (seen.contains(num - diff) && seen.contains(num - 2 * diff)) {
+                count++;
             }
+            seen.add(num);
         }
+        
         return count;
     }
 }

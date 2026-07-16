b363aa49372c659f00fee008f6ce30b6321f8f82
K Mahesh Babu <maheshbabu75k7@gmail.com>
2026-07-16 18:20:48 +0530

    Time: 3 ms (18.84%), Space: 43.4 MB (63.58%) - LeetHub

---
+++ 2395-find-subarrays-with-equal-sum/2395-find-subarrays-with-equal-sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean findSubarrays(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            int sum1 = nums[i] + nums[i + 1];
+            for (int j = i + 1; j < n - 1; j++) {
+                int sum2 = nums[j] + nums[j + 1];
+                if (sum1 == sum2) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}

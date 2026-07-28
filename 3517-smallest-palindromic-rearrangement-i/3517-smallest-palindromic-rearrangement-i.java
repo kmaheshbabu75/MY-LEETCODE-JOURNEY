df2f3af7c164dbab636d44c431a524834261505c
K Mahesh Babu <maheshbabu75k7@gmail.com>
2026-07-28 23:21:04 +0530

    Time: 33 ms (43.78%), Space: 48.4 MB (23.69%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
@@ -1,6 +1,5 @@
 class Solution {
     public String smallestPalindrome(String s) {
-        // Step 1: Count character frequencies
         int[] counts = new int[26];
         for (char c : s.toCharArray()) {
             counts[c - 'a']++;
@@ -9,23 +8,19 @@ class Solution {
         StringBuilder leftHalf = new StringBuilder();
         String middle = "";
         
-        // Step 2: Build the left half and find the middle character (if any)
         for (int i = 0; i < 26; i++) {
             if (counts[i] == 0) continue;
             
-            // If the count is odd, this character goes in the middle
             if (counts[i] % 2 != 0) {
                 middle = String.valueOf((char) (i + 'a'));
             }
             
-            // Append half of the occurrences to the left half
             int halfCount = counts[i] / 2;
             for (int j = 0; j < halfCount; j++) {
                 leftHalf.append((char) (i + 'a'));
             }
         }
         
-        // Step 3: Combine left half, middle, and reversed left half
         String left = leftHalf.toString();
         String right = leftHalf.reverse().toString();
         

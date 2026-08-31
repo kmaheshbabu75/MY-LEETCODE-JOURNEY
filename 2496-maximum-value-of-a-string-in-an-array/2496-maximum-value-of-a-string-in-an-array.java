8f14a75a38d1357c4c67d19c29dce00d0c09afb2
K Mahesh Babu <maheshbabu75k7@gmail.com>
2026-08-31 23:48:31 +0530

    Time: 7 ms (10.93%), Space: 44.3 MB (10.93%) - LeetHub

--- 2496-maximum-value-of-a-string-in-an-array/2496-maximum-value-of-a-string-in-an-array.java
+++ 2496-maximum-value-of-a-string-in-an-array/2496-maximum-value-of-a-string-in-an-array.java
@@ -1,17 +1,32 @@
+// class Solution {
+//     public int maximumValue(String[] strs) {
+//         int maxVal = 0;
+        
+//         for (String s : strs) {
+//             boolean isNumeric = true;
+//             for (char c : s.toCharArray()) {
+//                 if (c < '0' || c > '9') {
+//                     isNumeric = false;
+//                     break;
+//                 }
+//             }
+//             int val = isNumeric ? Integer.parseInt(s) : s.length();
+//             maxVal = Math.max(maxVal, val);
+//         }
+        
+//         return maxVal;
+//     }
+// }
 class Solution {
     public int maximumValue(String[] strs) {
         int maxVal = 0;
         
         for (String s : strs) {
-            boolean isNumeric = true;
-            for (char c : s.toCharArray()) {
-                if (c < '0' || c > '9') {
-                    isNumeric = false;
-                    break;
-                }
+            if (s.matches("\\d+")) {
+                maxVal = Math.max(maxVal, Integer.parseInt(s));
+            } else {
+                maxVal = Math.max(maxVal, s.length());
             }
-            int val = isNumeric ? Integer.parseInt(s) : s.length();
-            maxVal = Math.max(maxVal, val);
         }
         
         return maxVal;

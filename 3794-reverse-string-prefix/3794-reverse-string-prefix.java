dc6b858dcd8b5d589b1d4c9b11e30fe3786ce9da
K Mahesh Babu <maheshbabu75k7@gmail.com>
2026-09-01 23:53:34 +0530

    Time: 2 ms (43.86%), Space: 44.9 MB (23.33%) - LeetHub

--- 3794-reverse-string-prefix/3794-reverse-string-prefix.java
+++ 3794-reverse-string-prefix/3794-reverse-string-prefix.java
@@ -1,19 +1,25 @@
 class Solution {
     public String reversePrefix(String s, int k) {
+        // if (k <= 1) {
+        //     return s;
+        // }
+        // char[] chars = s.toCharArray();
+        // int left = 0;
+        // int right = k - 1;
+        // while (left < right) {
+        //     char temp = chars[left];
+        //     chars[left] = chars[right];
+        //     chars[right] = temp;
+        //     left++;
+        //     right--;
+        // }
+        
+        // return new String(chars);
         if (k <= 1) {
             return s;
         }
-        char[] chars = s.toCharArray();
-        int left = 0;
-        int right = k - 1;
-        while (left < right) {
-            char temp = chars[left];
-            chars[left] = chars[right];
-            chars[right] = temp;
-            left++;
-            right--;
-        }
         
-        return new String(chars);
+        StringBuilder prefix = new StringBuilder(s.substring(0, k)).reverse();
+        return prefix.append(s.substring(k)).toString();
     }
 }

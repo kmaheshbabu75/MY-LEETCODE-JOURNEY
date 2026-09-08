a6f8ddd15375d033b4be2dd94c985a3f46f21883
K Mahesh Babu <maheshbabu75k7@gmail.com>
2026-09-08 23:50:04 +0530

    Time: 2 ms (43.71%), Space: 44.5 MB (80.29%) - LeetHub

--- 3794-reverse-string-prefix/3794-reverse-string-prefix.java
+++ 3794-reverse-string-prefix/3794-reverse-string-prefix.java
@@ -12,8 +12,7 @@ class Solution {
         //     chars[right] = temp;
         //     left++;
         //     right--;
-        // }
-        
+        // } 
         // return new String(chars);
         StringBuilder prefix = new StringBuilder(s.substring(0, k)).reverse();
         return prefix.append(s.substring(k)).toString();

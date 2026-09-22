db1bb4a7e81a90bc4102bf5d537a1268c870201c
K Mahesh Babu <maheshbabu75k7@gmail.com>
2026-09-22 23:52:02 +0530

    Time: 33 ms (70.09%), Space: 59.1 MB (59.81%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,8 +1,6 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        
-        // 1. Sort the array to easily use two pointers and skip duplicates
         Arrays.sort(nums);
         
         for (int i = 0; i < nums.length - 2; i++) {

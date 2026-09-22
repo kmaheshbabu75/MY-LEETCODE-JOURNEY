d689f3fc34527082c107254b7adfee24973d3822
K Mahesh Babu <maheshbabu75k7@gmail.com>
2026-09-23 00:04:22 +0530

    Time: 35 ms (40.8%), Space: 58.5 MB (94.41%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -9,7 +9,6 @@ class Solution {
             }
             int left = i + 1;
             int right = nums.length - 1;
-            
             while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
                 if (sum == 0) {
@@ -25,7 +24,6 @@ class Solution {
                 }
             }
         }
-        
         return result;
     }
 }
